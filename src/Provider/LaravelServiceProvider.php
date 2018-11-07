5b6ce6c80f04446494be93110f27b6dd54a9e6cb
Mior Muhammad Zaki <crynobone@gmail.com>
2018-11-07 09:57:39 +0800

    Fixes CS.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Provider/LaravelServiceProvider.php
+++ src/Provider/LaravelServiceProvider.php
@@ -164,7 +164,7 @@ class LaravelServiceProvider extends DingoServiceProvider
     /**
      * Initialize the form request with data from the given request.
      *
-     * @param FormRequest $form
+     * @param FormRequest       $form
      * @param IlluminateRequest $current
      *
      * @return void
