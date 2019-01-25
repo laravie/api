5292f72246a03e1af73bccae306200f983b72751
Mior Muhammad Zaki <crynobone@gmail.com>
2019-01-25 09:53:04 +0800

    Update docblock.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Http/InternalRequest.php
+++ src/Http/InternalRequest.php
@@ -4,6 +4,9 @@ namespace Dingo\Api\Http;
 
 class InternalRequest extends Request
 {
+    /**
+     * {@inheritDoc}
+     */
     public function __construct(array $query = [], array $request = [], array $attributes = [], array $cookies = [], array $files = [], array $server = [], $content = null)
     {
         parent::__construct($query, $request, $attributes, $cookies, $files, $server, $content);
